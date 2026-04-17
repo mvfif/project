ae5e265b04760d9059a08d4ec7d8c3c9ebd4abea
mvfif <masha.shishkina2006@gmail.com>
2026-04-17 23:16:38 +0500

    решена проблема с cors

---
+++ main.py
@@ -0,0 +1,11 @@
+from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import FileResponse
+
+app = FastAPI()
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+@app.get("/")
+def read_index():
+    return FileResponse("static/index.html")
